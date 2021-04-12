cd17c2e831f517c0485b358d737535607e6f6b05
dmp11 <pd8@mail.ru>
2021-04-12 23:08:45 +0300

    feat: completed task 05-delete-digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,19 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let toArr = (`${n}`).split('');
+  let newNum = 0;
+  let endNum = 0;
+  for (let i = 0; i < toArr.length; i++) {
+    toArr.splice(i, 1);
+    newNum = toArr.join('') * 1;
+    if (newNum > endNum) {
+      endNum = newNum;
+    }
+    toArr = (`${n}`).split('');
+  }
+  return endNum;
 }
 
 module.exports = deleteDigit;
